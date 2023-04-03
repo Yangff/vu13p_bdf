950bca2183bcf29c5e2373aca74922e7b9fe9105
Yangff <yangff1@gmail.com>
2023-04-02 21:04:56 -0500

    fix scr to src

--- make.bat
+++ make.bat
@@ -2,4 +2,4 @@ xsdata ./dtd/board.dtd --package generated.board
 xsdata ./dtd/preset.dtd --package generated.preset
 xsdata ./dtd/part0_pins.dtd --package generated.part0_pins
 
-python3 ./scr/main.py
+python3 ./src/main.py
