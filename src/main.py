8e8d86fffceaf4f39774f1606d5a3bee21ec15d4
Yangff <yangff1@gmail.com>
2023-04-04 18:02:10 -0500

    add qsfp

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 import os
 from pathlib import Path
 import sys
-from xdc import parse_xdc
+from typing import List
+from xdc import parse_xdc # type: ignore
 
 COMPANY_NAME = "A company"
 # override company name with environment variable
 if "COMPANY_NAME" in os.environ:
     COMPANY_NAME = os.environ["COMPANY_NAME"]
+
+URL = "http://"
+# override url with environment variable
+if "URL" in os.environ:
+    URL = os.environ["URL"]
+
+FILE_VERSION = "1.0"
+# override file version with environment variable
+if "FILE_VERSION" in os.environ:
+    FILE_VERSION = os.environ["FILE_VERSION"]
 
 # change cwd to project root
 project_root = Path(__file__).parent.parent
@@ -56,10 +67,10 @@ def add_pin(name, loc, iostd = None):
     pins.pin.append(pin)
 
 # define vu13p board information
-vu13p = Board(display_name="Virtex UltraScale+ VU13P Accelerator Board", name="vu13p", url="http://", schema_version="2.2", vendor=COMPANY_NAME, preset_file="preset.xml", description = "Virtex UltraScale+ VU13P Accelerator Board")
+vu13p = Board(display_name="Virtex UltraScale+ VU13P Accelerator Board", name="vu13p", url=URL, schema_version="2.2", vendor=COMPANY_NAME, preset_file="preset.xml", description = "Virtex UltraScale+ VU13P Accelerator Board")
 vu13p.images = Images( [Image(name="vu13p.jpeg", display_name="vu13p Board", sub_type="board")] )
 vu13p.compatible_board_revisions = CompatibleBoardRevisions( [Revision(value = "1.0", id = "0")] )
-vu13p.file_version = "1.0"
+vu13p.file_version = FILE_VERSION
 vu13p.jumpers = Jumpers()
 
 part0 = Component(name="part0", display_name="Virtex UltraScale+ VU13P Accelerator Board", type=ComponentType.FPGA, part_name="xcvu13p-fhgb2104-2-i", pin_map_file="part0_pins.xml", vendor=COMPANY_NAME)
@@ -83,7 +94,7 @@ def reverse_name_tc(name):
         return name[:-1] + ('t' if name.endswith('c') else 'c')
     return name
 
-def map_xdc_ports(logical_port, physical_port, portname_xdc, direction, ports_data, iostd_data, reverse=no_reverse):
+def map_xdc_ports(logical_port, physical_port, portname_xdc, direction, ports_data, iostd_data, reverse=no_reverse, lanelimit=None):
     component_port = reverse(portname_xdc)
     port_map = PortMap(logical_port = logical_port, physical_port = physical_port, dir = PortMapDir(direction))
     port_map.pin_maps = PinMaps()
@@ -91,7 +102,10 @@ def map_xdc_ports(logical_port, physical_port, portname_xdc, direction, ports_da
     reverse_pin = lambda x:x
     if reverse != no_reverse:
         reverse_pin = get_diffpair
-    if len(pin_data) <= 1:
+    pin_len = len(pin_data)
+    if lanelimit and pin_len > lanelimit:
+        pin_len = lanelimit
+    if pin_len <= 1:
         pin_map = PinMap()
         pin_map.port_index = '0'
         pin_map.component_pin = component_port
@@ -102,8 +116,10 @@ def map_xdc_ports(logical_port, physical_port, portname_xdc, direction, ports_da
             add_pin(component_port, reverse_pin(pin_data[0]))
     else:
         port_map.left = "0"
-        port_map.right = str(len(pin_data) - 1)
+        port_map.right = str(pin_len - 1)
         for ind, pin in pin_data.items():
+            if ind >= pin_len:
+                break
             pin_map = PinMap()
             pin_map.port_index = str(ind)
             pin_map.component_pin = component_port + str(ind)
@@ -241,7 +257,133 @@ clk.port_maps = PortMaps([
 ])
 clkpin["c1_end_index"] = str(gIndex - 1)
 
-# TODO: qfp interface
+# qfp interface
+
+qsfp_portnames = {
+    "qsfp1": {
+        "txp": [("c2c_master_tx_txp", 2, range(0, 2)), ("c2c_slave_tx_txp", 2, range(2, 4))],
+        "rxp": [("c2c_master_rx_rxp", 2, range(0, 2)), ("c2c_slave_rx_rxp", 2, range(2, 4))],
+        "txn": [("c2c_master_tx_txn", 2, range(0, 2)), ("c2c_slave_tx_txn", 2, range(2, 4))],
+        "rxn": [("c2c_master_rx_rxn", 2, range(0, 2)), ("c2c_slave_rx_rxn", 2, range(2, 4))],
+        "clkp": "up_qsfp161_clk_p",
+        "clkn": "up_qsfp161_clk_n",
+        "i2c_sda": "up_qsfp_i2c_sda",
+        "i2c_scl": "up_qsfp_i2c_scl",
+    },
+    "qsfp2": {
+        "txp": [("dn_qsfp{idx}_tx_p", 4, range(0, 4))],
+        "rxp": [("dn_qsfp{idx}_rx_p", 4, range(0, 4))],
+        "txn": [("dn_qsfp{idx}_tx_n", 4, range(0, 4))],
+        "rxn": [("dn_qsfp{idx}_rx_n", 4, range(0, 4))],
+        "clkp": "dn_qsfp161_clk_p",
+        "clkn": "dn_qsfp161_clk_n",
+        "i2c_sda": "dn_qsfp_i2c_sda",
+        "i2c_scl": "dn_qsfp_i2c_scl",
+    }
+}
+
+qsfp_ports_data = xdcs['vu13p_qsfp']['ports']
+qsfp_iostd_data = xdcs['vu13p_qsfp']['iostd']
+
+new_qsfp_ports_data = {}
+new_qsfp_iostd_data = {}
+
+for component_name, portdata in qsfp_portnames.items():
+    for port in ("txp", "rxp", "txn", "rxn"):
+        # rename ports_data
+        new_port = f"{component_name}_{port}"
+        new_qsfp_ports_data[new_port] = {}
+        for oldname, sz, oldrange in portdata[port]:
+            for old_idx, new_idx in zip(range(0, sz), oldrange):
+                mapoldname = oldname.format(idx=old_idx)
+                if mapoldname != oldname:
+                    new_qsfp_ports_data[new_port][new_idx] = qsfp_ports_data[mapoldname][0]
+                else:
+                    new_qsfp_ports_data[new_port][new_idx] = qsfp_ports_data[mapoldname][old_idx]
+    for port in ("clkp", "clkn", "i2c_sda", "i2c_scl"):
+        new_qsfp_ports_data[f"{component_name}_{port}"] = qsfp_ports_data[portdata[port]]
+        if portdata[port] in qsfp_iostd_data:
+            new_qsfp_iostd_data[f"{component_name}_{port}"] = qsfp_iostd_data[portdata[port]]
+
+def g_name(port):
+    # tuen xxp to GXX_p
+    return ("g" + port[:-1] + "_" + port[-1]).upper()
+
+qsfp_pins = {
+    "qsfp1": {},
+    "qsfp2": {}
+}
+
+qsfp_clk_pins = {
+    "qsfp1": {},
+    "qsfp2": {}
+}
+
+def qsfp_preferip(lane):
+    if lane % 2 == 0:
+        preferred_ips = PreferredIps([
+            PreferredIp(vendor="xilinx.com", library="ip", name="xxv_ethernet", order="0"),
+            PreferredIp(vendor="xilinx.com", library="ip", name="l_ethernet", order="0"),
+            PreferredIp(vendor="xilinx.com", library="ip", name="interlaken", order="1")
+        ])
+        if lane == 4:
+            preferred_ips.preferred_ip.append(
+                PreferredIp(vendor="xilinx.com", library="ip", name="cmac_usplus", order="0")
+            )
+    else:
+        preferred_ips = PreferredIps([
+            PreferredIp(vendor="xilinx.com", library="ip", name="xxv_ethernet", order="0"),
+            PreferredIp(vendor="xilinx.com", library="ip", name="interlaken", order="1")
+        ])
+    return preferred_ips
+
+def qsfp_ipname(lane):
+    preferredips = qsfp_preferip(lane)
+    ipname = [ip.name for ip in preferredips.preferred_ip]
+    return ipname
+
+def filter_ip(lane, iplist: List[Ip]):
+    ipname = qsfp_ipname(lane)
+    return [ip for ip in iplist if ip.name in ipname]
+
+for component_name, _ in qsfp_portnames.items():
+    qsfp_pins[component_name]['c1_st_index'] = str(gIndex)
+    for lane in range(1, 5):
+        interface_name = f"{component_name}_{lane}x"
+        qsfp_interface = Interface(mode=InterfaceMode.MASTER, name=interface_name, type="xilinx.com:interface:gt_rtl:1.0", of_component=component_name, preset_proc=f"{interface_name}_preset")
+        part0.interfaces.interface.append(qsfp_interface)
+        qsfp_interface.description = f"{lane}-lane GT interface over QSFP"
+        qsfp_interface.preferred_ips = qsfp_preferip(lane)
+
+        port_maps = PortMaps()
+        qsfp_interface.port_maps = port_maps
+        for port, direction in [("txp", "out"), ("rxp", "in"), ("txn", "out"), ("rxn", "in")]:
+            port_maps.port_map.append(
+                map_xdc_ports(logical_port=g_name(port), physical_port=f"{component_name}_{port}{lane}", portname_xdc=f"{component_name}_{port}", direction=direction, ports_data=new_qsfp_ports_data, iostd_data=new_qsfp_iostd_data, lanelimit=lane)
+            )
+    qsfp_pins[component_name]['c1_end_index'] = str(gIndex - 1)
+
+    qsfp_clk_pins[component_name]['c1_st_index'] = str(gIndex)
+
+    # clk interface name
+    qclk_interface_name = f"{component_name}_eeg2102_clk"
+    # 161.132M
+    clk_interface = Interface(mode=InterfaceMode.SLAVE, name=qclk_interface_name, type="xilinx.com:interface:diff_clock_rtl:1.0", of_component=component_name)
+    part0.interfaces.interface.append(clk_interface)
+    clk_interface.parameters = Parameters([Parameter(name="frequency", value="161132000")])
+    clk_interface.preferred_ips = PreferredIps([
+        PreferredIp(vendor="xilinx.com", library="ip", name="xxv_ethernet", order="0"),
+        PreferredIp(vendor="xilinx.com", library="ip", name="l_ethernet", order="1"),
+        PreferredIp(vendor="xilinx.com", library="ip", name="interlaken", order="2"),
+        PreferredIp(vendor="xilinx.com", library="ip", name="cmac_usplus", order="3"),
+    ])
+    port_maps = PortMaps()
+    clk_interface.port_maps = port_maps
+    port_maps.port_map.append(map_xdc_ports("CLK_P", qclk_interface_name + "_p", f"{component_name}_clkp", "in", new_qsfp_ports_data, new_qsfp_iostd_data))
+    port_maps.port_map.append(map_xdc_ports("CLK_N", qclk_interface_name + "_p", f"{component_name}_clkn", "in", new_qsfp_ports_data, new_qsfp_iostd_data))
+    qsfp_clk_pins[component_name]['c1_end_index'] = str(gIndex)
+    # TODO: I2C??
+
 
 
 # Components and connections
@@ -253,7 +395,7 @@ for i in range(0, 4):
         type=ComponentType.CHIP, sub_type="ddr", 
         major_group="External Memory", 
         part_name="MT40A512M16HA-083E",
-        vendor="Micron", spec_url="https://media-www.micron.com/-/media/client/global/documents/products/data-sheet/dram/ddr4/8gb_ddr4_sdram.pdf?rev=74679247a1e24e57b6726071152f1384")
+        vendor="Micron", spec_url="https://media-www.micron.com/-/media/client/global/documents/products/data-sheet/dram/ddr4/8gb_ddr4_sdram.pdf")
     vu13p.components.component.append(component_ddr)
     component_ddr.parameters = Parameters([
         Parameter(name="ddr_type", value="ddr4"),
@@ -308,6 +450,28 @@ component_pcie.component_modes = ComponentModes([
                     ])),
 ])
 
+## qsfp
+
+for component_id in (1, 2):
+    component_name = f"qsfp{component_id}"
+    component_qsfp = Component(
+        name=component_name,
+        type=ComponentType.CHIP, sub_type="sfp", major_group="Ethernet Configurations", part_name="Unknown", vendor="Unknown", spec_url="www.marvell.com",
+        display_name=f"QSFP Connector {component_id}",
+        description=f"QSFP Connector {component_id}",
+    )
+    vu13p.components.component.append(component_qsfp)
+    component_qsfp.component_modes = ComponentModes()
+    for lane in range(1, 5):
+        mode_name = f"qsfp{component_id}_{lane}x"
+        mode = ComponentMode(name=mode_name, display_name=mode_name)
+        qclk_interface_name = f"{component_name}_eeg2102_clk"
+        mode.interfaces = Interfaces([
+            Interface(name=mode_name),
+            Interface(name=qclk_interface_name, optional=InterfaceOptional.TRUE),
+        ])
+        mode.preferred_ips = qsfp_preferip(lane)
+
 ## jtag_chains
 vu13p.jtag_chains = JtagChains(
     JtagChain(name="chain1", position=Position(name="0", component="part0"))
@@ -342,7 +506,13 @@ vu13p.connections = Connections([
             c1_end_index=clkpin['c1_end_index'],
             c2_st_index="0",
             c2_end_index="1")]
-    )]
+    )] + 
+    [Connection(name=f"part0_qsfp{component_id}_gt", component1=ConnectionComponent1.PART0, component2=f"qsfp{component_id}", connection_map=[
+        ConnectionMap(name=f"part0_qsfp{component_id}", c1_st_index=qsfp_pins[f"qsfp{component_id}"]['c1_st_index'], c1_end_index=qsfp_pins[f"qsfp{component_id}"]['c1_end_index'], c2_st_index="0", c2_end_index=mdd(qsfp_pins[f"qsfp{component_id}"]))
+    ]) for component_id in (1, 2)] + 
+    [Connection(name=f"part0_qsfp{component_id}_eeg2102_clk", component1=ConnectionComponent1.PART0, component2=f"qsfp{component_id}_eeg2102_clk", connection_map=[
+        ConnectionMap(name=f"part0_qsfp{component_id}_eeg2102_clk", c1_st_index=qsfp_clk_pins[f"qsfp{component_id}"]['c1_st_index'], c1_end_index=qsfp_clk_pins[f"qsfp{component_id}"]['c1_end_index'], c2_st_index="0", c2_end_index=mdd(qsfp_clk_pins[f"qsfp{component_id}"]))
+    ]) for component_id in (1, 2)] 
 )
 
 # ip associated rule
@@ -376,10 +546,33 @@ ip_associated_rule.ip.extend([
         for idx, ifname in enumerate(ifclks) ]) ]
     )])
 
+# gt_serial_port
+gtIps = (
+    "xxv_ethernet", "interlaken", "l_ethernet", "cmac_usplus"
+)
+for ip in gtIps:
+    ipLane = list(filter(lambda lane:ip in qsfp_ipname(lane), range(1, 5)))
+    order = 0
+    ip_associated_rule.ip.append(
+        B.Ip(vendor="xilinx.com", library="ip", name=ip, version="*", ip_interface="gt_serial_port",
+            associated_board_interfaces=[AssociatedBoardInterfaces([
+                AssociatedBoardInterface(name=f"qsfp{component_id}_{lane}x", order=f"{(order:=order+1)-1}")
+            for component_id in (1, 2) for lane in ipLane])]
+        )
+    )
+    order = 0
+    ip_associated_rule.ip.append(
+        B.Ip(vendor="xilinx.com", library="ip", name=ip, version="*", ip_interface="gt_ref_clk",
+            associated_board_interfaces=[AssociatedBoardInterfaces([
+                AssociatedBoardInterface(name=f"qsfp{component_id}_eeg2102_clk", order=f"{(order:=order+1)-1}")
+            for component_id in (1, 2) ])]
+        )
+    )
+
 # system configuration
 
-IpPresets = IpPresets(schema="1.0")
-IpPresets.ip_preset = [
+ipPresets = IpPresets(schema="1.0")
+ipPresets.ip_preset = [
     IpPreset(preset_proc_name="pcie_refclk_preset", ip = [
         Ip(vendor="xilinx.com", library="ip", name="util_ds_buf", user_parameters = UserParameters([
             UserParameter(name="CONFIG.C_BUF_TYPE", value="IBUFDSGTE"),
@@ -398,6 +591,7 @@ IpPresets.ip_preset = [
             UserParameter(name="CONFIG.ADDN_UI_CLKOUT1_FREQ_HZ", value="100"),
         ])),
     ]),
+# pciex16_preset
     IpPreset(preset_proc_name="pciex16_preset", ip = [
         Ip(vendor="xilinx.com", library="ip", name="xdma", user_parameters = UserParameters([
             UserParameter(name="CONFIG.pl_link_cap_max_link_width", value="X16"),
@@ -437,7 +631,73 @@ IpPresets.ip_preset = [
     ]),
 ]
 
-# pciex16_preset
+
+gt_quad = {
+    "qsfp1": list(range(52, 55 + 1)), # 233
+    "qsfp2": list(range(36, 39 + 1)), # 229
+}
+
+def gt_selection(comp, lane):
+    quad = gt_quad[comp]
+    st = quad[0]
+    if lane == 1:
+        return f"X1Y{st}"
+    ed = quad[lane - 1]
+    return f"X1Y{st}~X1Y{ed}"
+
+def quad_group(comp, _):
+    quad = gt_quad[comp]
+    st = quad[0]
+    return f"Quad_X1Y{st}"
+
+ilkne_selection = {
+    "qsfp1": "ILKNE4_X1Y7",
+    "qsfp2": "ILKNE4_X1Y5",
+}
+
+cmac_selection = {
+    "qsfp1": "CMACE4_X0Y11",
+    "qsfp2": "CMACE4_X0Y7",
+}
+
+for component_name in ("qsfp1", "qsfp2"):
+    for lane in range(1, 5):
+        preset_proc_name = f"{component_name}_{lane}x_preset"
+        ip_preset = IpPreset(preset_proc_name=preset_proc_name)
+        ipPresets.ip_preset.append(ip_preset)
+        ip_preset.ip = filter_ip(lane, [
+            Ip(vendor="xilinx.com", library="ip", name="xxv_ethernet", user_parameters = UserParameters([
+                UserParameter(name = "CONFIG.LINE_RATE", value = "10"), #
+                UserParameter(name = "CONFIG.GT_REF_CLK_FREQ", value = "161.132"), #
+                UserParameter(name = "CONFIG.NUM_OF_CORES", value = str(lane)),
+                UserParameter(name = "CONFIG.GT_TYPE", value = "GTY"),
+                UserParameter(name = "CONFIG.GT_GROUP_SELECT", value = quad_group(component_name, lane))
+            ])),
+            Ip(vendor="xilinx.com", library="ip", name="l_ethernet", user_parameters = UserParameters([
+                UserParameter(name = "CONFIG.LINE_RATE", value = "40"), #
+                UserParameter(name = "CONFIG.GT_REF_CLK_FREQ", value = "161.132"), #
+                UserParameter(name = "CONFIG.GT_TYPE", value = "GTY"),
+                UserParameter(name = "CONFIG.GT_GROUP_SELECT", value = quad_group(component_name, lane))
+            ])),
+            Ip(vendor="xilinx.com", library="ip", name="interlaken", user_parameters = UserParameters([
+                UserParameter(name = "CONFIG.LINE_RATE", value = "10.3125"), #
+                UserParameter(name = "CONFIG.GT_REF_CLK_FREQ", value = "161.1328125"), # 
+                UserParameter(name = "CONFIG.NUM_LANES", value = str(lane)),
+                UserParameter(name = "CONFIG.GT_TYPE", value = "GTY"),
+                UserParameter(name = "CONFIG.GT_SELECT", value = gt_selection(component_name, lane)),
+                UserParameter(name = "CONFIG.ILKN_CORE_LOC", value = "ILKNE4_X1Y8")
+            ])),
+            Ip(vendor="xilinx.com", library="ip", name="cmac_usplus", user_parameters = UserParameters([
+                UserParameter(name = "CONFIG.GT_TYPE", value = "GTY"),
+                UserParameter(name = "CONFIG.CMAC_CAUI4_MODE", value = "1"),
+                UserParameter(name = "CONFIG.NUM_LANES", value = f"{lane}x25"), #
+                UserParameter(name = "CONFIG.GT_REF_CLK_FREQ", value = "161.1328125"), #
+                UserParameter(name = "CONFIG.CMAC_CORE_SELECT", value = cmac_selection[component_name]),
+                UserParameter(name = "CONFIG.GT_GROUP_SELECT", value = gt_selection(component_name, lane)),
+                UserParameter(name = "CONFIG.RS_FEC_TRANSCODE_BYPASS", value = "0"),
+            ])),
+        ])
+
 # Save to xml
 from xsdata.formats.dataclass.serializers.config import SerializerConfig
 from xsdata.formats.dataclass.serializers import XmlSerializer
@@ -452,7 +712,7 @@ with open("result/Acompany/vu13p/1.0/board.xml", "w") as f:
     f.write(serializer.render(vu13p))
 
 with open("result/Acompany/vu13p/1.0/presets.xml", "w") as f:
-    f.write(serializer.render(IpPresets))
+    f.write(serializer.render(ipPresets))
 
 # copy board image file to result
 import shutil
